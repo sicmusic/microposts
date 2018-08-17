81b5b0aa99447a36ec0d7047636d8298247c9fc3
Paul <sicmusicmu@gmail.com>
2018-08-17 16:48:57 +0200

    starterpack

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const greeting = 'Hello World';
+console.log(greeting);
+
+const getData = async (url) => {
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+};
+
+getData('https://jsonplaceholder.typicode.com/posts/');
+
+
